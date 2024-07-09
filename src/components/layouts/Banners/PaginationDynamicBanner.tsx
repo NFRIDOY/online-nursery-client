77a6eae08b4522f4b85bf466cf34753f7407e66e
NF RIDOY <nfridoy@gmail.com>
2024-07-10 01:58:31 +0600

    [feat] (banner text)

--- src/components/layouts/Banners/PaginationDynamicBanner.tsx
+++ src/components/layouts/Banners/PaginationDynamicBanner.tsx
@@ -14,33 +14,46 @@ import { bannersLinks } from "../../../utils/bannersLinks";
 
 const PaginationDynamicBanner = () => {
     return (
-        <div>
-            <Swiper
-                pagination={{
-                    dynamicBullets: true,
-                }}
-                loop={true}
-                effect={"fade"}
-                autoplay={{
-                    delay: 2500,
-                    disableOnInteraction: false,
-                    waitForTransition: false
-                }}
-                speed={1000}
-                modules={[Pagination, EffectFade, Autoplay]}
-                className="mySwiper h-[calc(100vh-80px)] ">
-                {bannersLinks.map((banner) => (
-                    <>
-                        <SwiperSlide>
-                            <img
-                                src={banner}
-                                alt={banner}
-                                className="object-contain w-full"
-                            />
-                        </SwiperSlide>
-                    </>
-                ))}
-            </Swiper>
+        <div className="relative">
+            <div>
+                <Swiper
+                    pagination={{
+                        dynamicBullets: true,
+                    }}
+                    loop={true}
+                    effect={"fade"}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                        waitForTransition: false,
+                    }}
+                    speed={1000}
+                    modules={[Pagination, EffectFade, Autoplay]}
+                    className="mySwiper h-[calc(100vh-80px)] ">
+                    {bannersLinks.map((banner) => (
+                        <>
+                            <SwiperSlide>
+                                <img
+                                    src={banner}
+                                    alt={banner}
+                                    className="object-contain w-full"
+                                />
+                            </SwiperSlide>
+                        </>
+                    ))}
+                </Swiper>
+            </div>
+            <div className="absolute text-7xl top-[30%] left-20 z-50 w-1/2">
+                <h1>
+                    Let's Make{" "}
+                    <span className=" text-white font-bold">Beautiful</span>{" "}
+                    Plants A Part Of <br />
+                    <span>Your Life</span>
+                </h1>
+                <div>
+                    <button className="btn btn-accent">Shop Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/utils/bannersLinks.ts
+++ src/utils/bannersLinks.ts
@@ -1,7 +1,7 @@
 
 export const bannersLinks = [
     "https://images.unsplash.com/reserve/bOvf94dPRxWu0u3QsPjF_tree.jpg?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1502082553048-f009c37129b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1477554193778-9562c28588c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // "https://images.unsplash.com/photo-1502082553048-f009c37129b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // "https://images.unsplash.com/photo-1477554193778-9562c28588c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://plus.unsplash.com/premium_photo-1667419102563-e212046376c4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ]
