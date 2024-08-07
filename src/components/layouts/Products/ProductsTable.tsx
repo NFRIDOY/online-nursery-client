3f44cc2eeddcd0d3de78a89d11d433ef20c774d7
NF RIDOY <nfridoy@gmail.com>
2024-08-07 16:59:36 +0600

    [feat] (search in manag product table)

--- src/components/layouts/Products/ProductsTable.tsx
+++ src/components/layouts/Products/ProductsTable.tsx
@@ -1,19 +1,82 @@
 import { useEffect, useState } from "react";
 import useAxios from "../../../hooks/useAxios";
+import { useQuery } from "@tanstack/react-query";
+import Title from "../../ui/Title";
+import { RiDeleteBack2Fill } from "react-icons/ri";
+import { FaSearch } from "react-icons/fa";
 
 const ProductsTable = () => {
     const [productList, setProductList] = useState([]);
+    const [input, setInput] = useState("");
+
     const publicAxios = useAxios();
-    useEffect(() => {
-        publicAxios.get("/products").then((res) => {
-            setProductList(res?.data?.data);
-            // console.log(res?.data?.data);
-        });
-    }, []);
+    // useEffect(() => {
+    //     publicAxios.get("/products").then((res) => {
+    //         setProductList(res?.data?.data);
+    //         // console.log(res?.data?.data);
+    //     });
+    // }, []);
+
+    const {
+        isPending: isPendingProducts,
+        error: errorProducts,
+        data: productData,
+        refetch: refetchProducts,
+    } = useQuery({
+        queryKey: ["products", input],
+        queryFn: async () =>
+            await publicAxios
+                .get(`/products?searchTerm=${input}`)
+                .then((res) => {
+                    return res.data;
+                }),
+    });
+
+    const handleChange = (value) => {
+        setInput(value);
+        // fetchData(value);
+        
+    };
+
     return (
         <table className="table table-xs lg:table lg:table-lg">
             {/* head */}
             <thead>
+                <tr>
+                    <td colSpan={5}>
+                        <Title>
+                            {/* <input type="text" placeholder="Type to search..." /> */}
+                            <label className="input input-primary input-bordered flex items-center gap-2 mx-3 md:mx-10">
+                                <input
+                                    type="text"
+                                    className="grow"
+                                    placeholder="Search Plant Name"
+                                    value={input}
+                                    onChange={(e) =>
+                                        handleChange(e.target.value)
+                                    }
+                                />
+                                <button
+                                    type="button"
+                                    className="search-button hidden lg:inline">
+                                    {input ? (
+                                        <RiDeleteBack2Fill
+                                            size={20}
+                                            className="text-primary"
+                                            onClick={() => handleChange("")}
+                                        />
+                                    ) : (
+                                        // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-100 text-warning"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                                        <FaSearch
+                                            size={20}
+                                            className="text-warning"
+                                        />
+                                    )}
+                                </button>
+                            </label>
+                        </Title>
+                    </td>
+                </tr>
                 <tr>
                     <th>Name</th>
                     <th>Category & Stock</th>
@@ -24,7 +87,7 @@ const ProductsTable = () => {
             </thead>
             <tbody>
                 {/* row 1 */}
-                {productList?.map((product) => (
+                {productData?.data?.map((product) => (
                     <tr>
                         <td>
                             <div className="flex items-center gap-3">
--- src/pages/Admin/ManageProducts.tsx
+++ src/pages/Admin/ManageProducts.tsx
@@ -1,11 +1,13 @@
 import ProductsTable from "../../components/layouts/Products/ProductsTable";
+import Title from "../../components/ui/Title";
 
 const ManageProducts = () => {
     return (
         <div className="overflow-y-auto">
             <div className="flex gap-x-5">
                 <div className="overflow-y-auto h-full w-full">
-                    <h1 className="text-center text-2xl mt-5">Plants</h1>
+                    {/* <h1 className="text-center text-2xl mt-5"></h1> */}
+                    <Title>Plants</Title>
                     <ProductsTable />
                 </div>
             </div>
